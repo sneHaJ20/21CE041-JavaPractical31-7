1a13ba6f7adc0bc8066d5dffaf91ee6330975134
sneHaJ20 <109842070+sneHaJ20@users.noreply.github.com>
2022-07-31 22:37:40 +0530

    Update Practical4.java

--- Practical4.java
+++ Practical4.java
@@ -1,3 +1,6 @@
+//21CE041_SnehaJadeja
+//Given an array of ints, return true if the sequence of numbers 1, 2, 3 appears in the array somewhere.
+//array123([1, 1, 2, 3, 1]) → true   array123([1, 1, 2, 4, 1]) → false   array123([1, 1, 2, 1, 2, 3]) → true
 package Practical1;
 
 import java.util.Scanner;
