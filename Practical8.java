bd6da50aec1dc8741a11c00f35968f9e06efa0da
sneHaJ20 <109842070+sneHaJ20@users.noreply.github.com>
2022-07-31 22:39:04 +0530

    Update Practical8.java

--- Practical8.java
+++ Practical8.java
@@ -1,4 +1,12 @@
-package Practical1;
+/* 21CE041 Sneha Jadeja
+Aim-The problem is to write a program that will grade multiple-choice tests. Assume there are
+eight students and ten questions, and the answers are stored in a two-dimensional array. Each
+row records a student’s answers to the questions, as shown in the following array.
+Key to the Questions:0 1 2 3 4 5 6 7 8 9
+Key D B D C C D A E A D
+DYour program grades the test and displays the result. It compares each student’s answers 
+with the key, counts the number of correct answers, and displays it.
+package Practical1;*/
 
 public class Practical8 {
     public static void main(String[] args) {
