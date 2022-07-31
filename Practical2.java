9259682bebc376d5ed2e3f71f97c1f9e2485fad5
sneHaJ20 <109842070+sneHaJ20@users.noreply.github.com>
2022-07-31 22:34:13 +0530

    Update Practical2.java

--- Practical2.java
+++ Practical2.java
@@ -1,3 +1,7 @@
+// 21CE041_SnehaJadeja
+//Aim-  Given a string, return a string made of the first 2 chars (if present), 
+//however include first  char  only  if  it  is  'o' and  include  the  second  only  if  it
+//is  'z', so  "ozymandias" yields "oz",startOz("ozymandias") → "oz"startOz("bzoo") → "z"startOz("oxx") → "o"
 package Practical1;
 
 import java.util.Scanner;
