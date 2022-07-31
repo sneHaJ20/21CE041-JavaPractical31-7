bea2769761705b01b318c031dd1dfbb87f2f04cd
sneHaJ20 <109842070+sneHaJ20@users.noreply.github.com>
2022-07-31 22:39:30 +0530

    Update Practical6.java

--- Practical6.java
+++ Practical6.java
@@ -1,3 +1,8 @@
+//21CE041_SnehaJadeja
+//Aim- Given an array of strings, return a new array without the strings that are equal to the target string. One approach is to count the occurrences of the target
+/*string, make a new array of the correct length, and then copy over the correct 
+strings.wordsWithout(["a", "b", "c", "a"], "a") → ["b", "c"] 
+wordsWithout(["a", "b", "c", "a"], "b") → ["a", "c", "a"]wordsWithout(["a", "b", "c", "a"], "c") → ["a", "b", "a"]*/
 package Practical1;
 
 import java.util.Scanner;
